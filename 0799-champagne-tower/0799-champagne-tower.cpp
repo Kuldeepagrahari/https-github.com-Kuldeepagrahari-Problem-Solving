825845d80d37476d42ac8862bf14e49a238e7610
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2026-02-15 01:05:17 +0530

    Time: 7 ms (44.25%), Space: 10.8 MB (73.84%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>> dp(100, vector<double>(100, 0.0));
-        dp[0][0] = double(poured);
+        vector<double> cur(100, 0.0);
+        vector<double> nxt(100, 0.0);
+        cur[0] = poured;
         
         for(int row = 0; row < 99; row++) {
             for(int col = 0; col <= row; col++) {
-                vector<int> left_child = {row + 1, col};
-                vector<int> right_child = {row + 1, col + 1};
+                int left_child = col;
+                int right_child = col + 1;
 
-                double avail = dp[row][col];
+                double avail = cur[col];
 
                 if(avail > 1.0) {
                     double div = (avail - 1.0) / 2.0;
-                    dp[left_child[0]][left_child[1]] += div;
-                    dp[right_child[0]][right_child[1]] += div;
-                    dp[row][col] = 1.0;
+                    nxt[left_child] += div;
+                    nxt[right_child] += div;
+                    cur[col] = 1.0;
                 }
+
+                if(row == query_row && col == query_glass) return cur[col];
             }
+            cur = nxt;
+            nxt.assign(100, 0.0);
         }
 
         for(int col = 0; col < 100; col++) {
-            dp[99][col] = dp[99][col] >= 1.0 ? 1.0 : dp[99][col];
+            cur[col] = cur[col] >= 1.0 ? 1.0 : cur[col];
+            if(query_row == 99 && col == query_glass) return cur[col];
         }
-        return dp[query_row][query_glass];
+        return 0.0;
 
     }
 };
