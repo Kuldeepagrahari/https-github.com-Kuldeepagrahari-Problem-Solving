890efbf5190b26f3ccbea1f187247517bf2002ec
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2026-02-22 19:54:50 +0530

    Time: 35 ms (27.57%), Space: 65.1 MB (40.35%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,12 +4,19 @@ public:
         
         int n = nums.size();
 
-        // Approach 1
-        unordered_set<int> st;
-        for(int x: nums) {
-            if(st.find(x) != st.end())
-                return x;
-            st.insert(x);
+        // Approach 1 -> Using O(n) space
+        // unordered_set<int> st;
+        // for(int x: nums) {
+        //     if(st.find(x) != st.end())
+        //         return x;
+        //     st.insert(x);
+        // }
+
+        // Approach 2 -> using O(nlogn) time complexity
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n - 1; i++) {
+            if(nums[i] == nums[i + 1]) 
+                return nums[i];
         }
 
         return 0;
