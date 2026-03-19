322e67dc83d378f485e79d7fd6891c714e94d24c
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2026-03-19 16:40:42 +0530

    Time: 51 ms (70.73%), Space: 129.4 MB (69.92%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -7,18 +7,15 @@ public:
 
         vector<vector<pair<int, int>>> preCnt(n, vector<pair<int, int>>(m));
 
-
         for(int i = 0; i < n; i++) {
             int rowXCnt = 0;
             int rowYCnt = 0;
 
             for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 'X') {
+                if(grid[i][j] == 'X') 
                     rowXCnt ++;
-                }
-                else if(grid[i][j] == 'Y') {
+                else if(grid[i][j] == 'Y') 
                     rowYCnt++;
-                }
 
                 int xCnt = (i >= 1 ? preCnt[i - 1][j].first : 0) + rowXCnt;
                 int yCnt = (i >= 1 ? preCnt[i - 1][j].second : 0) + rowYCnt;
