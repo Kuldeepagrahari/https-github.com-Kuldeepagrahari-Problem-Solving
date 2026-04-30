885c441c969543fe24e987b1462faf41aaab20c1
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2026-05-01 04:35:36 +0530

    Time: 1044 ms (21.12%), Space: 499.5 MB (47.12%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     int solve(int row, int col, int cost, int k, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
-        int n = grid.size(), m = grid[0].size();
         
-        // 1. Out of bounds check
-        if (row >= n || col >= m) return -1e9; 
+        int loc_cost =  (grid[row][col] == 0 ? cost : cost + 1);
+        int n = grid.size(), m = grid[0].size();
 
-        // 2. Update current cost: if current cell is 1, increment cost
-        int current_cost = (grid[row][col] == 0) ? cost : cost + 1;
+        if(loc_cost > k)
+            return -1e9;
 
-        // 3. Pruning: If cost exceeds k, this path is dead
-        if (current_cost > k) return -1e9;
-
-        // 4. Base Case: Reached the bottom-right
-        if (row == n - 1 && col == m - 1) {
-            return grid[row][col];
+        if(row == n - 1 && col == m - 1) {
+            return grid[n - 1][m - 1];
         }
 
-        // 5. Memoization check
-        if (dp[row][col][current_cost] != -1) 
-            return dp[row][col][current_cost];
-
-        // 6. Recursive calls
-        int right_score = solve(row, col + 1, current_cost, k, grid, dp);
-        int down_score = solve(row + 1, col, current_cost, k, grid, dp);
-
-        // 7. Calculate result: current cell value + max of paths
-        int res = grid[row][col] + max(right_score, down_score);
+        if(dp[row][col][loc_cost] != -1) 
+            return dp[row][col][loc_cost];
         
-        return dp[row][col][current_cost] = res;
-    }
+        int right_score = -1e9, down_score = -1e9;
 
+        // go to right
+        if(col + 1 < m)
+            right_score = grid[row][col] + solve(row, col + 1, loc_cost, k, grid, dp);
+            
+        if(row + 1 < n)
+            down_score = grid[row][col] + solve(row + 1, col, loc_cost, k, grid, dp);
+    
+
+        return dp[row][col][loc_cost] = max(right_score, down_score);
+
+    }
     int maxPathScore(vector<vector<int>>& grid, int k) {
         int n = grid.size(), m = grid[0].size();
-        // Initialize DP with -1
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k + 1, -1)));
-        
+
         int ans = solve(0, 0, 0, k, grid, dp);
-        
-        // If the result is a very small number, no valid path was found
-        return (ans < 0) ? -1 : ans;
+
+        if(ans < 0)
+            return -1;
+
+        return ans;
     }
 };
