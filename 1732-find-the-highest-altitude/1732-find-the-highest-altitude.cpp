9c89c2ff95859476a501ec38e885eba8ee138eec
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2026-06-19 15:06:06 +0530

    Time: 0 ms (100%), Space: 10.7 MB (73.87%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int ans = 0;
+        int sum = 0;
+
+        for(int &x: gain) {
+            sum += x;
+            ans = max(ans, sum);
+        }
+
+        return ans;
+    }
+};
