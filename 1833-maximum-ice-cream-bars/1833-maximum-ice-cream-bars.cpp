1f6d72c54b1d8dc58510ae7e4ff45384f8b29832
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2026-06-21 23:50:12 +0530

    Time: 0 ms (100%), Space: 84.3 MB (19.23%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -3,13 +3,29 @@ public:
     int maxIceCream(vector<int>& costs, int coins) {
         int n = costs.size();
         int bars = 0;
-        sort(costs.begin(), costs.end());
-        int i = 0;
 
-        while(i < n && coins - costs[i] >= 0) {
-            coins -= costs[i];
-            bars++;
-            i++;
+        // Type1 Sorting Algo: In built Sorting -> O(NlogN)
+        // sort(costs.begin(), costs.end());
+
+        // Type2 sorting Algo: Count 
+        int maxElem = 0;
+        for(int i = 0; i < n; i++) {
+            if(costs[i] > maxElem) {
+                maxElem = costs[i];
+            }
+        }
+        vector<int> freq(maxElem + 1, 0);
+        for(int i = 0; i < n; i++) {
+            freq[costs[i]]++;
+        }
+
+        for(int c = 1; c <= maxElem; c++) {
+            if(freq[c] > 0) {
+                while(freq[c]-- && coins - c >= 0) {
+                    coins -= c;
+                    bars++;
+                }
+            }
         }
 
         return bars;
