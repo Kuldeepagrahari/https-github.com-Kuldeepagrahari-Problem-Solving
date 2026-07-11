ac000a67857209e4663f4f4f308226c21b27d5f8
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2026-07-11 10:59:36 +0530

    Time: 101 ms (8.61%), Space: 134.7 MB (46.55%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void dfs(int u, int &num_vert, int &num_edges, vector<bool> &vis, vector<vector<int>> &adj) {
+        vis[u] = true;
+        num_edges += adj[u].size();
+        num_vert++;
+
+        for(int v: adj[u]) {
+            if(vis[v] == false) {
+                dfs(v, num_vert, num_edges, vis, adj);
+            }
+        }
+        return;
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+
+        for(auto it: edges) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<bool> vis(n, false);
+        int num_connected_comp = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(vis[i] == false) {
+                int num_vert = 0, num_edges = 0;
+                dfs(i, num_vert, num_edges, vis, adj);
+                cout << num_vert << " " << num_edges << endl;
+                if(num_vert * (num_vert - 1) == num_edges) {
+                    num_connected_comp++;
+                }
+            }
+        }
+
+        return num_connected_comp;
+    }
+};
