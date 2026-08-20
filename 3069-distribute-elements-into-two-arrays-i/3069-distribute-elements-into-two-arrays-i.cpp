c9b40cc6436372aa70dd81b5403661c57534c0a7
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2026-08-20 23:19:21 +0530

    Time: 0 ms (100%), Space: 23.6 MB (97.31%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
         int n = nums.size();
-
         vector<int> result(n);
+
+        // Step1: use result array from L->R for arr1 and R -> L for arr2
+        // because we dont know the size of the arr1 and arr2
         result[0] = nums[0];
         result[n - 1] = nums[1];
-        int j1 = 1, j2 = n - 2;
-        int i1 = 0, i2 = 1;
+        int idx1 = 1, idx2 = n - 2;
+        int lastIdx1 = 0, lastIdx2 = 1;
 
         for(int idx = 2; idx < n; idx++) {
-            if(nums[i1] > nums[i2]) {
-                i1 = idx;
-                result[j1++] = nums[i1];
+            if(nums[lastIdx1] > nums[lastIdx2]) {
+                lastIdx1 = idx;
+                result[idx1++] = nums[lastIdx1];
             }
             else {
-                i2 = idx;
-                result[j2--] = nums[i2];
+                lastIdx2 = idx;
+                result[idx2--] = nums[lastIdx2];
             }
         }
 
-        // reverse arr2
-        j2++;
-        int rev2 = n - 1;
-        int n2 = n - j2;
-        for(int idx = 0; idx < n2/2; idx++) {
-            swap(result[j2++], result[rev2--]);
-        }
-
+        // Step2: reverse arr2 
+        idx2++;
+        int revIdx2 = n - 1;
+        while(idx2 < revIdx2)
+            swap(result[idx2++], result[revIdx2--]);
 
         return result;
     }
