3e31e3b6424b7bc44e8b3800e194f352c13fcd16
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2026-09-10 11:12:55 +0530

    Time: 59 ms (34.25%), Space: 54.8 MB (63.78%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    const size = functions.length
+
+    return function(x) {
+        for(var i = size - 1; i >= 0; i--) {
+            x = functions[i](x);
+        }
+        return x;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
